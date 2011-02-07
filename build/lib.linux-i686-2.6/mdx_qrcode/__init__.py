5ad82207437067b882e96ee6e8a7c452225618fa
Zenobius Jiricek <airtonix@gmail.com>
2011-02-07 14:57:51 +1030

    working tag

---
+++ build/lib.linux-i686-2.6/mdx_qrcode/__init__.py
@@ -0,0 +1,2 @@
+from extension import *
+
--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from distutils.core import setup
 setup(
   name='markdown_qrcode',
-  version='0.0.2',
+  version='0.0.3.a',
   maintainer="Airtonix",
   maintainer_email="airtonix@gmail.com",
   url="airtonix.net/projects/markdown-qrcode",
   py_modules=[
     'mdx_qrcode',
+    'mdx_qrcode.extension',
     'mdx_qrcode.QrCodeLib',
   ],
-  license='LICENSE.txt',
+  license='LICENSE.md',
   description='A markdown extension to insert qrcode datauri images based on supplied data.',
-  long_description=open('README.txt').read(),
+  long_description=open('README.md').read(),
 )
 
